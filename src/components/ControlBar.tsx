3d2e8ecace9b7cda940876d25e43cd22f9c5da55
Dmitry Yudakov <dmitry.yudakov@gmail.com>
2020-04-10 21:02:27 +0300

    Ability to sign in and out from control bar

--- src/components/ControlBar.tsx
+++ src/components/ControlBar.tsx
@@ -2,16 +2,26 @@ import React, { useState } from 'react';
 import { ObjectItemInput } from '../types';
 import { AddNewChat } from './Chat';
 import { Login } from './Login';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 
 export const ControlBar: React.FC<{
   authenticated: boolean;
   onAdd: (item: ObjectItemInput) => Promise<any>;
 }> = ({ authenticated, onAdd }) => {
   const [addType, setAddType] = useState<ObjectItemInput['type'] | null>(null);
+  const [login, setLogin] = useState(false);
+
+  const showLogin = login || (!authenticated && !!addType);
+
+  const signOut = () => {
+    firebase.auth().signOut();
+    setLogin(false); // just in case
+  };
 
   return (
     <div id="control-bar">
-      {!authenticated && !!addType && <Login />}
+      {showLogin && <Login />}
       {authenticated && (
         <>
           {addType === 'chat' && (
@@ -24,6 +34,11 @@ export const ControlBar: React.FC<{
         </>
       )}
       <button onClick={() => setAddType('chat')}>New chat</button>
+      {authenticated ? (
+        <button onClick={signOut}>Sign out</button>
+      ) : (
+        <button onClick={() => setLogin(true)}>Register or sign in</button>
+      )}
     </div>
   );
 };
